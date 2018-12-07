326d4b29c68c8835cf6b3715d3335cf8949da591
Peter KETTIG <peter.kettig@cnes.fr>
2018-12-07 13:47:28 +0100

    Added function to retrieve product acquisition date

--- Common/DateConverter.py
+++ Common/DateConverter.py
@@ -29,3 +29,48 @@ def stringToDatetime(s):
     if(len(s) == 8):
         return dt.strptime(s, "%Y%m%d")
     raise ValueError("Unknown datetime string {0}".format(s))
+    
+    
+def getDateFromProduct(product, platform):
+    """
+    Get the acquisition date of a S2, L8 or Vns product using its filename
+    :param product: Tuple of product name and productID (0,1...) for different Regexes
+    :param platform: Platform ID (0 == S2, 1 == L8, 2 == Vns)
+    :return: The datetime of the product
+    :type return: datetime.datetime
+    """
+    from Common import DateConverter
+    import os
+    path, productType = product
+    filename = os.path.basename(path)
+    datetimeAsString = None
+    if(platform == 0):
+        if(productType == 0):
+            datetimeAsString = filename.split("_")[2].split("T")[0]
+        elif(productType == 1):
+            datetimeAsString = filename.split("_")[1].split("-")[0]
+        elif(productType == 2):
+            datetimeAsString = os.path.splitext(filename)[0].split("_")[-1]
+        elif(productType == 3):
+            datetimeAsString = filename.split("_")[5].split("T")[0]
+    elif(platform == 1):
+        if(productType == 0):
+            datetimeAsString = filename.split("_")[3]
+        elif(productType == 1):
+            #Special treatment here: Acq date is given as YYYYJJJ where JJJ
+            #is the Julian date of the year
+            from datetime import datetime
+            yeardoy = filename[9:16]
+            return datetime.strptime(yeardoy, '%Y%j').date()
+        elif(productType == 2):
+            datetimeAsString = filename.split("_")[1].split("-")[0]
+        elif(productType == 3):
+            datetimeAsString = os.path.splitext(filename)[0].split("_")[-1]
+    elif(platform == 2):
+        if(productType == 0):
+            datetimeAsString = filename.split("_")[1].split("-")[0]
+        elif(productType == 1):
+            datetimeAsString = os.path.splitext(filename)[0].split("_")[-1]
+    if(datetimeAsString == None):
+        raise ValueError("Unknown platform found for product %s" % filename)
+    return DateConverter.stringToDatetime(datetimeAsString)
--- test/Common/test_DateConverter.py
+++ test/Common/test_DateConverter.py
@@ -22,3 +22,27 @@ class testDateConverter(LoggedTestCase.LoggedTestCase):
        self.assertEqual(DateConverter.datetimeToString(origAsDT),
                         orig)
        
+    @testFunction.test_function
+    def test_PlatformDates(self):
+        from Common import DateConverter as dc
+        s2 = [(["S2A_MSIL1C_20170412T110621_N0204_R137_T29RPQ_20170412T111708.SAFE", 0], 0),
+              (["SENTINEL2B_20171008-105012-463_L1C_T31TCH_C_V1-0", 1], 0),
+              (["S2B_MSIL1C_20180316T103021_N0206_R108_T32TMR_20180316T123927.SAFE", 0], 0)]
+        datesS2 = ["20170412", "20171008", "20180316"]
+        l8 = [(["LANDSAT8-OLITIRS-XSTHPAN_20170501-103532-111_L1C_T31TCH_C_V1-0",2], 1),
+              (["LC80390222013076EDC00", 1], 1),
+              (["LC08_L1TP_199029_20170527_20170615_01_T1", 0], 1)]
+        datesL8 = ["20170501", "20130317", "20170527", "20130626"]
+        vns = [(["VENUS-XS_20180201-051359-000_L1C_KHUMBU_C_V1-0", 0], 2),
+               (["VE_VM01_VSC_L2VALD_ISRAW906_20180317.HDR", 1], 2),
+               (["VE_TEST_VSC_L1VALD_CAMARGUE_20120101.HDR", 1], 2)]
+        datesVns = ["20180201", "20180317", "20120101"]
+        for (prod, platform), date in zip(s2, datesS2):
+            d = dc.datetimeToStringShort(dc.getDateFromProduct(prod, platform))
+            self.assertEqual(date, d)
+        for (prod, platform), date in zip(l8, datesL8):
+            d = dc.datetimeToStringShort(dc.getDateFromProduct(prod, platform))
+            self.assertEqual(date, d)
+        for (prod, platform), date in zip(vns, datesVns):
+            d = dc.datetimeToStringShort(dc.getDateFromProduct(prod, platform))
+            self.assertEqual(date, d)
