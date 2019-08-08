ffb87d0c6a8698d48968e0525b0493cc0e35a8a0
Peter KETTIG <peter.kettig@cnes.fr>
2019-08-08 16:54:15 +0200

    Removed product specific date functions

--- Common/DateConverter.py
+++ Common/DateConverter.py
@@ -2,11 +2,13 @@
 
 from datetime import datetime as dt
 
+
 def datetimeToString(timestamp):
     """
     @brief Convert datetime to string %Y-%m-%dT%H:%M:%S.%f
     """
     return timestamp.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
+
 
 def datetimeToStringShort(timestamp):
     """
@@ -14,11 +16,13 @@ def datetimeToStringShort(timestamp):
     """
     return timestamp.strftime("%Y%m%d")
 
+
 def datetimeToStringName(timestamp):
     """
     @brief Convert datetime to string YYYYMMDD
     """
     return timestamp.strftime("%Y%m%d-%H%M%S-%f")[:-3]
+
 
 def stringToDatetime(s):
     """
@@ -29,94 +33,3 @@ def stringToDatetime(s):
     if(len(s) == 8):
         return dt.strptime(s, "%Y%m%d")
     raise ValueError("Unknown datetime string {0}".format(s))
-    
-def findPreviousL2Product(dates, pivot):
-    """
-    Find the nearest older date to pivot in the items list
-    :param items: List of datetimes
-    :param pivot: Reference datetime 
-    :return: Datetime in items closest to pivot or None if not existing
-    """
-    # Search only on the days older that pivot:
-    past_dates = findOlderProducts(dates, pivot)
-    if not past_dates:
-        return None
-    return min(past_dates, key=lambda x: abs(x[0] - pivot))
-    
-def findOlderProducts(dates, pivot):
-    """
-    Find all older dates to pivot in the items list
-    :param items: List of datetimes
-    :param pivot: Reference datetime 
-    :return: Datetimes older than pivot or None if not existing
-    """
-    past_dates = [(date, prod)
-                    for date, prod in dates
-                    if date < pivot]
-    return past_dates
-    
-def findNewerProducts(dates, pivot):
-    """
-    Find all newer dates to pivot in the items list
-    :param items: List of datetimes
-    :param pivot: Reference datetime 
-    :return: Datetimes newer than pivot or None if not existing
-    """
-    past_dates = [(date, prod)
-                    for date, prod in dates
-                    if date > pivot]
-    return past_dates
-    
-def getDateFromProduct(product, platform):
-    """
-    Get the acquisition date of a S2, L8 or Vns product using its filename
-    :param product: Tuple of product name and productID (0,1...) for different Regexes
-    :param platform: Platform ID (0 == S2, 1 == L8, 2 == Vns)
-    :return: The datetime of the product
-    :type return: datetime.datetime
-    """
-    import os
-    path, productType = product
-    filename = os.path.basename(path)
-    datetimeAsString = None
-    if(platform == 0):
-        if(productType == 0):
-            datetimeAsString = filename.split("_")[2].split("T")[0]
-        elif(productType == 1):
-            datetimeAsString = filename.split("_")[1].split("-")[0]
-        elif(productType == 2):
-            datetimeAsString = os.path.splitext(filename)[0].split("_")[-1]
-        elif(productType == 3):
-            datetimeAsString = filename.split("_")[5].split("T")[0]
-    elif(platform == 1):
-        if(productType == 0):
-            datetimeAsString = filename.split("_")[3]
-        elif(productType == 1):
-            #Special treatment here: Acq date is given as YYYYJJJ where JJJ
-            #is the Julian date of the year
-            from datetime import datetime
-            yeardoy = filename[9:16]
-            return datetime.strptime(yeardoy, '%Y%j').date()
-        elif(productType == 2):
-            datetimeAsString = filename.split("_")[1].split("-")[0]
-        elif(productType == 3):
-            datetimeAsString = os.path.splitext(filename)[0].split("_")[-1]
-    elif(platform == 2):
-        if(productType == 0):
-            datetimeAsString = filename.split("_")[1].split("-")[0]
-        elif(productType == 1):
-            datetimeAsString = os.path.splitext(filename)[0].split("_")[-1]
-    if(datetimeAsString == None):
-        raise ValueError("Unknown platform found for product %s" % filename)
-    return stringToDatetime(datetimeAsString)
-
-def getCAMSDate(filename):
-    """
-    Get the date from a cams filename.
-    Filename is something like:
-    S2__TEST_EXO_CAMS_YYYYMMDDTHHmmSS_YYYYMMDDTHHmmSS.HDR
-    With the first YYYYMMDDTHHmmSS being the date we are searching for
-    :param filename: The filename of a CAMS file
-    """
-    from os.path import splitext
-    return stringToDatetime(splitext(filename)[0].split("_")[-2].split("T")[0])
--- test/Common/test_DateConverter.py
+++ test/Common/test_DateConverter.py
@@ -7,42 +7,18 @@ from Common import DateConverter
 class TestDateConverter(unittest.TestCase):
 
     def testDateConversionShort(self):
-       orig = "20180522"
-       origAsDT = DateConverter.stringToDatetime(orig)
-       self.assertEqual(DateConverter.datetimeToStringShort(origAsDT), orig)
-       self.assertEqual(DateConverter.datetimeToString(origAsDT),
-                        "2018-05-22T00:00:00.000Z")
+        orig = "20180522"
+        origAsDT = DateConverter.stringToDatetime(orig)
+        self.assertEqual(DateConverter.datetimeToStringShort(origAsDT), orig)
+        self.assertEqual(DateConverter.datetimeToString(origAsDT),
+                         "2018-05-22T00:00:00.000Z")
        
     def testDateConversionLong(self):
-       orig = "2018-05-22T00:00:00.000Z"
-       origAsDT = DateConverter.stringToDatetime(orig)
-       self.assertEqual(DateConverter.datetimeToStringShort(origAsDT), "20180522")
-       self.assertEqual(DateConverter.datetimeToString(origAsDT),
-                        orig)
-       
-    def test_PlatformDates(self):
-        from Common import DateConverter as dc
-        s2 = [(["S2A_MSIL1C_20170412T110621_N0204_R137_T29RPQ_20170412T111708.SAFE", 0], 0),
-              (["SENTINEL2B_20171008-105012-463_L1C_T31TCH_C_V1-0", 1], 0),
-              (["S2B_MSIL1C_20180316T103021_N0206_R108_T32TMR_20180316T123927.SAFE", 0], 0)]
-        datesS2 = ["20170412", "20171008", "20180316"]
-        l8 = [(["LANDSAT8-OLITIRS-XSTHPAN_20170501-103532-111_L1C_T31TCH_C_V1-0",2], 1),
-              (["LC80390222013076EDC00", 1], 1),
-              (["LC08_L1TP_199029_20170527_20170615_01_T1", 0], 1)]
-        datesL8 = ["20170501", "20130317", "20170527", "20130626"]
-        vns = [(["VENUS-XS_20180201-051359-000_L1C_KHUMBU_C_V1-0", 0], 2),
-               (["VE_VM01_VSC_L2VALD_ISRAW906_20180317.HDR", 1], 2),
-               (["VE_TEST_VSC_L1VALD_CAMARGUE_20120101.HDR", 1], 2)]
-        datesVns = ["20180201", "20180317", "20120101"]
-        for (prod, platform), date in zip(s2, datesS2):
-            d = dc.datetimeToStringShort(dc.getDateFromProduct(prod, platform))
-            self.assertEqual(date, d)
-        for (prod, platform), date in zip(l8, datesL8):
-            d = dc.datetimeToStringShort(dc.getDateFromProduct(prod, platform))
-            self.assertEqual(date, d)
-        for (prod, platform), date in zip(vns, datesVns):
-            d = dc.datetimeToStringShort(dc.getDateFromProduct(prod, platform))
-            self.assertEqual(date, d)
+        orig = "2018-05-22T00:00:00.000Z"
+        origAsDT = DateConverter.stringToDatetime(orig)
+        self.assertEqual(DateConverter.datetimeToStringShort(origAsDT), "20180522")
+        self.assertEqual(DateConverter.datetimeToString(origAsDT),
+                         orig)
 
 
 if __name__ == '__main__':
