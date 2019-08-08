648e010ffd06bdcce9bce4d8bf378ed2af50be2f
Peter KETTIG <peter.kettig@cnes.fr>
2019-08-08 11:59:47 +0200

    Removed print decorator

--- Common/TestFunctions.py
+++ Common/TestFunctions.py
@@ -6,8 +6,6 @@ Created on Thu Jul  5 17:42:44 2018
 @author: akynos
 """
 
-import logging
-
 
 def touch(path):
     """
@@ -18,17 +16,3 @@ def touch(path):
     import os
     with open(path, 'a'):
         os.utime(path, None)
-
-
-def test_function(func):
-    """
-    Decorator to print the name of the function for debug purposes
-    """
-    def echo_func(*func_args, **func_kwargs):
-
-        logging.debug("==={0:=^30}===".format(func.__name__))
-        func(*func_args, **func_kwargs)
-
-        logging.debug("====================================")
-
-    return echo_func
