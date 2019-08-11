ef4884b0340137ed1839fc75b26bfb2d26baf61e
Peter KETTIG <peter.kettig@cnes.fr>
2019-08-11 11:20:55 +0200

    Added L1 and L2 generators

--- Chain/TestFiles.py
+++ Chain/TestFiles.py
@@ -9,19 +9,27 @@ import random
 import os
 
 
-def random_tile():
+def random_tile(platform="S2"):
     """
-    Create a string of the format T12ABC with 12 and ABC being chosen randomly.
-    :return: String compliant with reg T[0-9]{2}[A-Z]{3}
+    Create a random tile string for a given platform. For 'S2': T12ABC, for 'L8': 192820, for 'VE': ABCDE
+    :param platform: Return a tile corresponding to the given platform
+    :return: String compliant with the given platform
     """
     import string
     letters = string.ascii_uppercase
-    return "T" + str(random.randint(0, 99)).zfill(2) + ''.join(random.choice(letters) for _ in range(3))
+    tile = "T" + str(random.randint(0, 99)).zfill(2) + ''.join(random.choice(letters) for _ in range(3))
+    if platform == "S2":
+        return tile
+    elif platform == "L8":
+        number = str(random.randint(0, 999999)).zfill(6)
+        return random.choice([number, tile])
+    elif platform == "VE":
+        return ''.join(random.choice(letters) for _ in range(5))
 
 
 def random_platform(product_level=None):
     if not product_level:
-        product_level = random.choice(["L1C", "L2A", "L3A"])
+        product_level = random.choice(["L1C", "L2A"])
     platform_options = {"L1C": ["S2A", "S2B", "LANDSAT8", "VENUS"],
                         "L2A": ["SENTINEL2B", "SENTINEL2A", "LANDSAT8-OLITIRS", "VENUS-XS"]}
     return random.choice(platform_options[product_level])
@@ -40,11 +48,21 @@ class DummyGenerator(object):
     """
     Base class for all dummy generators
     """
-    def __init__(self, root, date=random_date(), tile=random_tile(), platform=random_platform()):
+    def __init__(self, root, date=random_date(), tile=None, platform=random_platform()):
         self.root = root
         self.date = date
-        self.tile = tile
-        self.platform = platform
+        if platform[:2].upper() == "S2":
+            self.platform = "S2"
+        elif platform[:2].upper() == "L8":
+            self.platform = "L8"
+        elif platform[:2].upper() == "VE":
+            self.platform = "VE"
+        else:
+            raise ValueError("Unknown platform found: %s" % platform)
+        if not tile:
+            self.tile = random_tile(platform=platform)
+        else:
+            self.tile = tile
 
     def generate(self, **kwargs):
         raise NotImplementedError
@@ -54,18 +72,6 @@ class DummyEarthExplorer(DummyGenerator):
     """
     Base class for all EarthExplorer-like files
     """
-
-    def __init__(self, root, date=random_date(), tile=random_tile(), platform=random_platform()):
-        super(DummyEarthExplorer, self).__init__(root, date, tile, platform)
-        if platform[:2].upper() == ["S2"] or platform[:9].upper() == "SENTINEL2":
-            self.platform = "S2"
-        elif platform[:2].upper() == ["L8"] or platform[:8].upper() == "LANDSAT8":
-            self.platform = "L8"
-        elif platform[:2].upper() == ["VE"] or platform[:8].upper() == "VENUS":
-            self.platform = "VE"
-        else:
-            raise ValueError("Unknown platform found: %s" % platform)
-
     def get_mission(self):
         """
         Return a random Mission name
@@ -199,3 +205,52 @@ class GippGenerator(DummyEarthExplorer):
                     continue
                 mission = "SENTINEL2"
                 self._create_hdr(sat, name, start_date, version, allsites, mission, ".EEF")
+
+
+class L1Generator(DummyGenerator):
+    """
+    Class to create a dummy L1C product
+    """
+    def __init__(self, root, date=random_date(), tile=None, platform=random_platform(product_level="L1C")):
+        super(DummyGenerator, self).__init__(root, date, tile, platform)
+
+    def generate(self, **kwargs):
+        from Common import TestFunctions
+        orbit = kwargs.get("orbit", random.randint(0, 999))
+        version_orbit = kwargs.get("version", random.randint(0, 9))
+        date_str = self.date.strftime("%Y%m%dT%H%M%S")
+        product_name = "_".join([self.platform,
+                                 "MSIL1C",
+                                 date_str,
+                                 "N" + str(orbit).zfill(4),
+                                 "R" + str(version_orbit).zfill(3),
+                                 self.tile,
+                                 date_str + ".SAFE"])
+        product_path = os.path.join(self.root, product_name)
+        metadata_path = os.path.join(product_path, "MTD_MSIL1C.xml")
+        os.makedirs(product_path)
+        TestFunctions.touch(metadata_path)
+
+
+class L2Generator(DummyGenerator):
+    """
+    Class to create a dummy L2A product
+    """
+    def __init__(self, root, date=random_date(), tile=None, platform=random_platform(product_level="L2A")):
+        super(DummyGenerator, self).__init__(root, date, tile, platform)
+
+    def generate(self, **kwargs):
+        from Common import TestFunctions
+        ms = kwargs.get("ms", random.randint(0, 999))
+        version = kwargs.get("version", random.randint(0, 9))
+        date_str = self.date.strftime("%Y%m%d-%H%M%S-") + str(ms)
+        product_name = "_".join([self.platform,
+                                 date_str,
+                                 "L2A",
+                                 self.tile,
+                                 random.choice("DC"),
+                                 "V" + str(version) + "-" + str(version)])
+        product_path = os.path.join(self.root, product_name)
+        metadata_path = os.path.join(product_path, product_name + "_MTD_ALL.xml")
+        os.makedirs(product_path)
+        TestFunctions.touch(metadata_path)
