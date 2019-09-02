649127ef9b0b659f21c909ef10af3b79b4da901f
Peter KETTIG <peter.kettig@cnes.fr>
2019-09-02 08:10:45 +0200

    Working on new DTMCreation

---
+++ prepare_mnt/srtm/SRTMFiles.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) CNES - All Rights Reserved
+This file is subject to the terms and conditions defined in
+file 'LICENSE.md', which is part of this source code package.
+
+Author:         Peter KETTIG <peter.kettig@cnes.fr>, Pierre LASSALLE <pierre.lassalle@cnes.fr>
+Project:        StartMaja, CNES
+Created on:     Tue Sep 11 15:31:00 2018
+"""
+
+
+class SRTMFiles(object):
+    """
+    Class to get the necessary srtm-files for a given site.
+    """
+
+    def __init__(self, site, dem_dir):
+        pass
+
+    def get_dem_data(self):
+        """
+        Get the DEM raw-data from the given directory. If not existing, an attempt will be made to download
+        it automatically.
+        :return:
+        """
+        pass
+
+
+if __name__ == "__main__":
+    pass
