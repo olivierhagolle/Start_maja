892cab9953e4d2d054c5a7fa5e05f2018cdbe7b9
Peter KETTIG <peter.kettig@cnes.fr>
2019-09-03 22:20:08 +0200

    Added new functions to imageIO

--- Common/ImageIO.py
+++ Common/ImageIO.py
@@ -6,86 +6,117 @@ Created on Wed Nov 22 15:03:42 2017
 @author: Peter Kettig
 """
 
-from osgeo import gdal, gdal_array
-import os
-import numpy as np
 
-import logging
-log = logging.getLogger(__name__)
-
-"""
-Functions to open TIFF/GeoTIFF images with GDAL and transforming them to numpy.arrays
-"""
-
-def fileExists(input_file):
-    """
-    @brief: Test if the path input_file exists
-    @param input_file The filepath
-    @return True, if file exists
-    """
-    return os.path.isfile(input_file)
-
-def openTiff(tiff_file):
+def open_tiff(tiff_file):
     """
     @brief: Opens a tiff file
     @param tiff_file The filepath of the .tiff
     @return A Gdal format file
     """
-    if fileExists(tiff_file):
-        log.debug("Opening file {0}".format(tiff_file))
+    from osgeo import gdal
+    import os
+    if os.path.exists(tiff_file):
         return gdal.Open(tiff_file)
     else:
         raise NameError("GDAL could not open file {0}".format(tiff_file))
 
-def tiffToArray(tiff_file, lon_offset_px=0, lat_offset_px=0, arrayOnly = True):
+
+def tiff_to_array(tiff_file, lon_offset_px=0, lat_offset_px=0, array_only=True):
     """
-    @brief Transforms tiff file into an array
-    @param tiff_file The File path
-    @param lon_offset_px Offset for image in x-Direction
-    @param lat_offset_px Offset for image in y-Direction
-    @return Numpy.array with shape (Bands,y-Index,x-Index)
-    
-    @Note: Bands index starts at 1, not at 0
+    Transforms tiff file into an array.
+    Note: Bands index starts at 1, not at 0
+    :param tiff_file: The File path
+    :param lon_offset_px: Offset for image in x-Direction
+    :param lat_offset_px: Offset for image in y-Direction
+    :param array_only: Numpy.array with shape (Bands,y-Index,x-Index)
+    :return:
     """
-    gdo = openTiff(tiff_file)
-    tiff_array = gdo.ReadAsArray(lon_offset_px,lat_offset_px)
-    if(arrayOnly):
+    gdo = open_tiff(tiff_file)
+    tiff_array = gdo.ReadAsArray(lon_offset_px, lat_offset_px)
+    if array_only:
         gdo = None
     return tiff_array, gdo
 
-def writeGTiff(dstImage, outputPath, projection, coordinates, dtype = None):
+
+def write_geotiff(img, dst, projection, coordinates, dtype=None):
     """
-    @brief Writes a GeoTiff file created from an existing coordinate-system
-    @Note Coordinates: [Top-Left X, W-E Resolution, 0, Top Left Y, 0, N-S Resolution]
+    Writes a GeoTiff file created from an existing coordinate-system
+    :param img: The numpy array to write
+    :param dst: The destination path
+    :param projection: The gdal projection
+    :param coordinates: The geotransform [Top-Left X, W-E Resolution, 0, Top Left Y, 0, N-S Resolution]
+    :param dtype: The desired gdal dtype
+    :return: Writes image to given path.
     """
+    from osgeo import gdal, gdal_array
+    import numpy as np
     driver = gdal.GetDriverByName('GTiff')
-    #Add dimension for a single band-image
-    if(dstImage.ndim is 2):
-        dstImage = dstImage[..., np.newaxis]  
-    #Set output dtype if not specified
-    if(dtype == None):
-        dtype = gdal_array.NumericTypeCodeToGDALTypeCode(dstImage.dtype)
-    dataset = driver.Create(outputPath, dstImage.shape[1], dstImage.shape[0] , dstImage.shape[2], dtype)
-    if(dataset == None):
-        raise OSError ("GDAL Could not create {0}".format(outputPath))
-    if(not coordinates or len(coordinates) != 6):
+    # Add dimension for a single band-image
+    if img.ndim is 2:
+        img = img[..., np.newaxis]
+    # Set output dtype if not specified
+    if not dtype:
+        dtype = gdal_array.NumericTypeCodeToGDALTypeCode(img.dtype)
+    dataset = driver.Create(dst, img.shape[1], img.shape[0], img.shape[2], dtype)
+    if not dataset:
+        raise OSError("GDAL Could not create file {0}".format(dst))
+    if not coordinates or len(coordinates) != 6:
         raise ValueError("Geotransform empty or unknown: {0}".format(coordinates))
     dataset.SetGeoTransform(coordinates)  
-    if(not projection):
+    if not projection:
         raise ValueError("Projection empty or unknown: {0}".format(projection))
     dataset.SetProjection(projection)
-    for index in range(dstImage.shape[2]):
-        dataset.GetRasterBand(index+1).WriteArray(dstImage[:,:,index])
+    for index in range(img.shape[2]):
+        dataset.GetRasterBand(index+1).WriteArray(img[:, :, index])
     dataset.FlushCache()  # Write to disk.
-    dataset = None
-    driver = None
-    return 0
 
-def writeGTiffExisting(dstImage, referenceFile, outputPath, dtype = None):
+
+def write_geotiff_existing(img, dst, driver, dtype=None):
     """
-    @brief: Create a GeoTiff image from an existing image
+    Create a GeoTiff image with an existing driver
     """
-    geotransform = referenceFile.GetGeoTransform()
-    projection = referenceFile.GetProjection()
-    #Write the new array
-    return writeGTiff(dstImage, outputPath, projection, geotransform)
+    geotransform = driver.GetGeoTransform()
+    projection = driver.GetProjection()
+    # Write the new array
+    return write_geotiff(img, dst, projection, geotransform, dtype)
+
+
+def get_epsg(driver):
+    """
+    Get the EPSG code from a given driver using gdal
+    :param driver: The gdal driver
+    :return: The EPSG code if existing.
+    """
+    from osgeo import gdal
+    return int(gdal.Info(driver, format='json')['coordinateSystem']['wkt'].rsplit('"EPSG","', 1)[-1].split('"')[0])
+
+
+def get_ul_lr(driver):
+    """
+    Get the coordinates of the upper left and lower right as tuples
+    :param driver: The gdal driver
+    :return: The ul and lr- coordinates in the projected coordinate system.
+    """
+    ulx, xres, xskew, uly, yskew, yres = driver.GetGeoTransform()
+    lrx = ulx + (driver.RasterXSize * xres)
+    lry = uly + (driver.RasterYSize * yres)
+    return (ulx, uly), (lrx, lry)
+
+
+def transform_point(point, driver, new_epsg=4326):
+    """
+    Transform a tuple (x,y) into lat lon using EPSG 4326
+    :param point: The point as tuple (x,y)
+    :param driver: The gdal driver of the given point
+    :param new_epsg: The EPSG code of the new coordinate system to transfer to. Default is 4326 (WGS84).
+    :return: The point's location in the new epsg as (x, y, z) with z usually being 0.0
+    """
+    from osgeo import osr
+    source = osr.SpatialReference()
+    source.ImportFromWkt(driver.GetProjection())
+
+    # The target projection
+    target = osr.SpatialReference()
+    target.ImportFromEPSG(new_epsg)
+    transform = osr.CoordinateTransformation(source, target)
+    return transform.TransformPoint(point[0], point[1])
--- test/Common/test_ImageIO.py
+++ test/Common/test_ImageIO.py
@@ -8,9 +8,75 @@ import os
 
 class TestFileIO(unittest.TestCase):
 
-    def testWriteGeoTiff(self):
+    def setUp(self):
+        self.coordinates = (652594.9112913811, 10.00887639510383, 0,
+                            5072876.717295351, 0, -9.974893672262251)
+        self.projection = 'PROJCS["WGS 84 / UTM zone 31N",\
+                           GEOGCS["WGS 84",DATUM["WGS_1984",\
+                           SPHEROID["WGS 84",6378137,298.257223563,\
+                           AUTHORITY["EPSG","7030"]],\
+                           AUTHORITY["EPSG","6326"]],\
+                           PRIMEM["Greenwich",0,\
+                           AUTHORITY["EPSG","8901"]],\
+                           UNIT["degree",0.0174532925199433,\
+                           AUTHORITY["EPSG","9122"]],\
+                           AUTHORITY["EPSG","4326"]],\
+                           PROJECTION["Transverse_Mercator"],\
+                           PARAMETER["latitude_of_origin",0],\
+                           PARAMETER["central_meridian",3],\
+                           PARAMETER["scale_factor",0.9996],\
+                           PARAMETER["false_easting",500000],\
+                           PARAMETER["false_northing",0],\
+                           UNIT["metre",1,AUTHORITY["EPSG","9001"]],\
+                           AXIS["Easting",EAST],AXIS["Northing",NORTH],\
+                           AUTHORITY["EPSG","32631"]]'
+        self.height = 200
+        self.width = 100
+
+    def test_write_read_geotiff(self):
+        from Common import FileSystem
+
+        img = np.ones((self.height, self.width), np.int16)
+        path = os.path.join(os.getcwd(), "test_write_read_geotiff.tif")
+        # Write Image and check return code
+        ImageIO.write_geotiff(img, path, self.projection, self.coordinates)
+        self.assertTrue(os.path.exists(path))
+        
+        img_read, driver = ImageIO.tiff_to_array(path, array_only=False)
+        self.assertTrue((img_read == img).all())
+        geotransform_read = driver.GetGeoTransform()
+        projection_read = driver.GetProjection()
+        self.assertEqual(geotransform_read, self.coordinates)
+        # Compare projections by removing all spaces cause of multiline string
+        self.assertEqual(projection_read.replace(" ", ""), self.projection.replace(" ", ""))
+        FileSystem.remove_file(path)
+        # Check if file removed
+        self.assertFalse(os.path.exists(path))
+        
+    def test_faulty_geotransform_projection(self):
+        from Common import FileSystem
+
+        coordinates = (652594.9112913811, 10.00887639510383, 0,
+                       5072876.717295351, 0)  # Missing one value
+        projection = ''
+        height = 200
+        width = 100
+        img = np.ones((height, width), np.int16)
+        path = os.path.join(os.getcwd(), "test_faulty_geotransform_projection.tif")
+        # Check geotransform wrong:
+        with self.assertRaises(ValueError):
+            ImageIO.write_geotiff(img, path, projection, coordinates)
         coordinates = (652594.9112913811, 10.00887639510383, 0,
                        5072876.717295351, 0, -9.974893672262251)
+        # Check projection wrong:
+        with self.assertRaises(ValueError):
+            ImageIO.write_geotiff(img, path, projection, coordinates)
+        FileSystem.remove_file(path)
+        self.assertFalse(os.path.exists(path))
+
+    def test_get_epsg(self):
+        from Common import FileSystem
+        epsg_ref = 32631
         projection = 'PROJCS["WGS 84 / UTM zone 31N",\
                         GEOGCS["WGS 84",DATUM["WGS_1984",\
                         SPHEROID["WGS 84",6378137,298.257223563,\
@@ -29,46 +95,52 @@ class TestFileIO(unittest.TestCase):
                         PARAMETER["false_northing",0],\
                         UNIT["metre",1,AUTHORITY["EPSG","9001"]],\
                         AXIS["Easting",EAST],AXIS["Northing",NORTH],\
-                        AUTHORITY["EPSG","32631"]]'
-        height = 200
-        width = 100
-        dstImage = np.ones((height, width), np.int16)
-        path = os.path.join(os.getcwd(), "testWriteGeoTiff.tif")
-        #Write Image and check return code
-        self.assertEqual(ImageIO.writeGTiff(dstImage, path, projection, coordinates), 0)
+                        AUTHORITY["EPSG","%s"]]' % epsg_ref
+        img = np.ones((self.height, self.width), np.int16)
+        path = os.path.join(os.getcwd(), "test_epsg.tif")
+        # Write Image and check return code
+        ImageIO.write_geotiff(img, path, projection, self.coordinates)
         self.assertTrue(os.path.exists(path))
-        
-        arrRead, driverRead = ImageIO.tiffToArray(path, arrayOnly=False)
-        self.assertTrue((arrRead == dstImage).all())
-        coordinatesRead = driverRead.GetGeoTransform()
-        projectionRead = driverRead.GetProjection()
-        self.assertEqual(coordinatesRead, coordinates)
-        #Compare projections by removing all spaces cause of multiline string
-        self.assertEqual(projectionRead.replace(" ", ""), projection.replace(" ", ""))
-        try:
-            os.remove(path)
-        except:
-            pass
-        #Check if file removed
-        self.assertFalse(os.path.exists(path))
-        
-    def testFaultyGeotransform(self):
-        coordinates = (652594.9112913811, 10.00887639510383, 0,
-                       5072876.717295351, 0) #Missing one value
-        projection = ''
-        height = 200
-        width = 100
-        dstImage = np.ones((height, width), np.int16)
-        path = os.path.join(os.getcwd(), "testWriteGeoTiff.tif")
-        #Check geotransform wrong:
-        with self.assertRaises(ValueError):
-            self.assertEqual(ImageIO.writeGTiff(dstImage, path, projection, coordinates), 0)
-        coordinates = (652594.9112913811, 10.00887639510383, 0,
-                       5072876.717295351, 0, -9.974893672262251)
-        #Check projection wrong:
-        with self.assertRaises(ValueError):
-            self.assertEqual(ImageIO.writeGTiff(dstImage, path, projection, coordinates), 0)
 
+        img_read, driver = ImageIO.tiff_to_array(path, array_only=False)
+        self.assertEqual(epsg_ref, ImageIO.get_epsg(driver))
+        FileSystem.remove_file(path)
+        # Check if file removed
+        self.assertFalse(os.path.exists(path))
+
+    def test_get_ul_lr(self):
+        from Common import FileSystem
+        img = np.ones((self.height, self.width), np.int16)
+        path = os.path.join(os.getcwd(), "test_get_ul_lr.tif")
+        # Write Image and check return code
+        ImageIO.write_geotiff(img, path, self.projection, self.coordinates)
+        self.assertTrue(os.path.exists(path))
+
+        img_read, driver = ImageIO.tiff_to_array(path, array_only=False)
+        ul, lr = ImageIO.get_ul_lr(driver)
+        self.assertEqual(ul, (652594.9112913811, 5072876.717295351))
+        self.assertEqual(lr, (653595.7989308914, 5070881.738560899))
+
+        FileSystem.remove_file(path)
+        # Check if file removed
+        self.assertFalse(os.path.exists(path))
+
+    def test_transform_point_lat_lon(self):
+        from Common import FileSystem
+        img = np.ones((self.height, self.width), np.int16)
+        path = os.path.join(os.getcwd(), "test_transform_point_lat_lon.tif")
+        # Write Image and check return code
+        ImageIO.write_geotiff(img, path, self.projection, self.coordinates)
+        self.assertTrue(os.path.exists(path))
+
+        img_read, driver = ImageIO.tiff_to_array(path, array_only=False)
+        center = (653095.355, 5071879.228)
+        lat_lon_expected = (4.9694934619557145, 45.78349724618419, 0.0)
+        lat_lon_calc = ImageIO.transform_point(center, driver, new_epsg=4326)
+        self.assertEqual(lat_lon_expected, lat_lon_calc)
+        FileSystem.remove_file(path)
+        # Check if file removed
+        self.assertFalse(os.path.exists(path))
 
 if __name__ == '__main__':
     unittest.main()
