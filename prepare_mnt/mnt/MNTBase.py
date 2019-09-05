26ba15d20fca3b157f7c22682f4ff1546743508c
Peter KETTIG <peter.kettig@cnes.fr>
2019-09-05 17:51:29 +0200

    Finished gsw code creation. Added gsw downloader

--- prepare_mnt/mnt/MNTBase.py
+++ prepare_mnt/mnt/MNTBase.py
@@ -57,6 +57,7 @@ class MNT(object):
     def __init__(self, site, dem_dir):
         self.site = site
         self.dem_dir = dem_dir
+        self.gsw_codes = self.get_gsw_codes(self.site)
 
     def get_raw_data(self):
         """
@@ -71,31 +72,56 @@ class MNT(object):
         """
         Get the list of GSW files for a given site.
         :param site: The site class
-        :return: The list of filenames needed in order to cover to whole site.
+        :return: The list of filenames of format 'XX(E/W)_YY(N/S)' needed in order to cover to whole site.
         """
         import math
-        num_x = [math.ceil(math.fabs(site.ul_latlon[0]) / 10) * 10, math.ceil(math.fabs(site.ul_latlon[1]) / 10) * 10]
-        if site.ul_latlon[0] > 0:
-            code_x1 = "N"
-        else:
-            code_x1 = "S"
-        if site.ul_latlon[1] < 0:
-            code_x2 = "W"
-        else:
-            code_x2 = "E"
-            num_x[1] -= 10
+        from collections import namedtuple
+        if site.ul_latlon[1] > 170 and site.lr_latlon[1] < 160:
+            raise ValueError("Cannot wrap around longitude change")
 
-        num_y = [math.ceil(math.fabs(site.lr_latlon[0]) / 10) * 10, math.ceil(math.fabs(site.lr_latlon[1]) / 10) * 10]
-        if site.lr_latlon[0] > 0:
-            code_y1 = "N"
-        else:
-            code_y1 = "S"
-        if site.lr_latlon[1] < 0:
-            code_y2 = "W"
-        else:
-            code_y2 = "E"
-            num_y[1] -= 10
-        return num_x, code_x1, code_x2, num_y, code_y1, code_y2
+        grid_step = 10
+        point = namedtuple("point", ("y", "x"))
+        pts = []
+        for pt in [site.ul_latlon, site.lr_latlon]:
+            lat_dec = (math.fabs(pt[0]) / grid_step)
+            lon_dec = (math.fabs(pt[1]) / grid_step)
+            if pt[0] > 0:
+                lat_id = int(math.ceil(lat_dec) * grid_step)
+            else:
+                lat_id = -1 * int(math.floor(lat_dec) * grid_step)
+
+            if pt[1] < 0:
+                lon_id = int(math.ceil(lon_dec) * grid_step)
+            else:
+                lon_id = -1 * int(math.floor(lon_dec) * grid_step)
+            pts.append(point(lat_id, lon_id))
+        gsw_granules = []
+        for x in range(pts[1].x, pts[0].x + grid_step, grid_step):
+            for y in range(pts[1].y, pts[0].y + grid_step, grid_step):
+                code_lat = "S" if y < 0 else "N"
+                code_lon = "W" if x > 0 else "E"
+                gsw_granules.append("%s%s_%s%s" % (int(math.fabs(x)), code_lon, int(math.fabs(y)), code_lat))
+        return gsw_granules
+
+    @staticmethod
+    def download_gsw(codes, dst):
+        """
+        Download the given gsw_codes
+        :param codes: The gsw codes of format 'XX(E/W)_YY(N/S)'
+        :param dst: The destination folder
+        :return: The list of filenames downloaded.
+        """
+        import os
+        from Common import FileSystem
+        url_base = "https://storage.googleapis.com/global-surface-water/downloads2/occurrence/occurrence_%s_v1_1.tif"
+        filenames = []
+        for code in codes:
+            current_url = url_base % code
+            filename = os.path.basename(current_url)
+            filenames.append(filename)
+            output_path = os.path.join(dst, filename)
+            FileSystem.download_file(current_url, output_path)
+        return filenames
 
     def get_water_data(self):
         raise NotImplementedError
--- test/prepare_mnt/mnt/test_MNTBase.py
+++ test/prepare_mnt/mnt/test_MNTBase.py
@@ -11,12 +11,13 @@ Created on:     Tue Dec  5 10:26:05 2018
 """
 
 import unittest
+import os
 from prepare_mnt.mnt import MNTBase
 
 
 class TestMNTBase(unittest.TestCase):
 
-    def atest_gsw_codes_toulouse(self):
+    def test_gsw_codes_toulouse(self):
         site = MNTBase.Site("T31TCJ", 32631,
                             ul=(300000.000, 4900020.000),
                             lr=(409800.000, 4790220.000),
@@ -24,7 +25,10 @@ class TestMNTBase(unittest.TestCase):
                             res_y=-10)
 
         gsw_codes = MNTBase.MNT.get_gsw_codes(site)
-        self.assertEqual(gsw_codes, [0, 1])
+        self.assertEqual(gsw_codes, ['0E_50N'])
+        fn = MNTBase.MNT.download_gsw(gsw_codes, os.getcwd())
+        self.assertEqual(len(fn), 1)
+        self.assertEqual(os.path.basename(fn[0]), "occurrence_0E_50N_v1_1.tif")
 
     def test_gsw_codes_spain(self):
         site = MNTBase.Site("T31TBE", 32631,
@@ -34,7 +38,37 @@ class TestMNTBase(unittest.TestCase):
                             res_y=-10)
 
         gsw_codes = MNTBase.MNT.get_gsw_codes(site)
-        self.assertEqual(gsw_codes, [0, 1])
+        self.assertEqual(gsw_codes, ['0E_40N', '0E_50N', '10W_40N', '10W_50N'])
+
+    def test_gsw_codes_zero_center(self):
+        site = MNTBase.Site("Somewhere_over_the_ocean", 32631,
+                            ul=(-250000.000, 250000.000),
+                            lr=(250000.000, -120000.000),
+                            res_x=10,
+                            res_y=-10)
+
+        gsw_codes = MNTBase.MNT.get_gsw_codes(site)
+        self.assertEqual(gsw_codes, ['0E_0N', '0E_10N', '10W_0N', '10W_10N'])
+
+    def test_gsw_codes_south_america(self):
+        site = MNTBase.Site("Ecuador", 32619,
+                            ul=(-250000.000, 250000.000),
+                            lr=(400000.000, -120000.000),
+                            res_x=10,
+                            res_y=-10)
+
+        gsw_codes = MNTBase.MNT.get_gsw_codes(site)
+        self.assertEqual(gsw_codes, ['70W_0N', '70W_10N', '80W_0N', '80W_10N'])
+
+    def test_gsw_codes_longitude_change(self):
+        site = MNTBase.Site("Russia", 32601,
+                            ul=(-250000.000, 250000.000),
+                            lr=(400000.000, -120000.000),
+                            res_x=10,
+                            res_y=-10)
+        # TODO Support this:
+        with self.assertRaises(ValueError):
+            MNTBase.MNT.get_gsw_codes(site)
 
 
 if __name__ == '__main__':
