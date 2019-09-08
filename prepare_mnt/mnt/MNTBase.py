662f92e022b0e781a23ec7fe234c682a57b24cfd
Peter KETTIG <peter.kettig@cnes.fr>
2019-09-08 19:16:58 +0200

    Moved Site and DEMInfo to separate files

--- prepare_mnt/mnt/MNTBase.py
+++ prepare_mnt/mnt/MNTBase.py
@@ -13,99 +13,6 @@ Created on:     Tue Sep 11 15:31:00 2018
 surface_water_url = "https://storage.googleapis.com/global-surface-water/downloads2/occurrence/occurrence_%s_v1_1.tif"
 
 
-class Site:
-    """
-    Stores all necessary information in order to create an MNT
-    """
-    def __init__(self, nom, epsg, px, py, ul, lr, res_x, res_y):
-        self.nom = nom
-        self.epsg = epsg
-        self.px = px
-        self.py = py
-        self.res_x = res_x
-        self.res_y = res_y
-        self.ul = ul
-        self.lr = lr
-        self.ul_latlon, self.lr_latlon = self.latlon_minmax
-
-    @property
-    def latlon_minmax(self):
-        """
-        Get lat and lon min and max values
-        :return: latmin, latmax, lonmin, lonmax of the current sites
-        """
-        from Common import ImageIO
-        ul_latlon = ImageIO.transform_point(self.ul, self.epsg, new_epsg=4326)
-        lr_latlon = ImageIO.transform_point(self.lr, self.epsg, new_epsg=4326)
-        return ul_latlon, lr_latlon
-
-    @property
-    def projwin(self):
-        return " ".join([str(self.ul[1]), str(self.ul[0]), str(self.lr[1]), str(self.lr[0])])
-
-    @property
-    def te_str(self):
-        y_val = list(sorted([self.lr[1], self.ul[1]]))
-        x_val = list(sorted([self.lr[0], self.ul[0]]))
-        return " ".join([str(x_val[0]), str(y_val[0]), str(x_val[1]), str(y_val[1])])
-
-    @property
-    def epsg_str(self):
-        return "EPSG:" + str(self.epsg)
-
-    @property
-    def tr_str(self):
-        return str(self.res_x) + " " + str(self.res_y)
-
-    def to_driver(self, path, n_bands=1):
-        from osgeo import osr, gdal
-        # create the raster file
-        dst_ds = gdal.GetDriverByName('GTiff').Create(path, self.py, self.px, n_bands, gdal.GDT_Byte)
-        geotransform = (self.ul[1], self.res_x, 0, self.ul[0], 0, self.res_y)
-        dst_ds.SetGeoTransform(geotransform)  # specify coords
-        srs = osr.SpatialReference()  # establish encoding
-        srs.ImportFromEPSG(self.epsg)  # WGS84 lat/long
-        dst_ds.SetProjection(srs.ExportToWkt())  # export coords to file
-        return dst_ds
-
-    @staticmethod
-    def from_raster(name, raster):
-        """
-        Create site from a raster on disk
-        :param name: The name of the site
-        :param raster: The gdal raster
-        :return: A site class given the infos from the raster.
-        """
-        from Common import ImageIO
-        raster, driver = ImageIO.tiff_to_array(raster, array_only=False)
-        nx, ny = raster.shape[:2]
-        epsg = ImageIO.get_epsg(driver)
-        ul, lr = ImageIO.get_ul_lr(driver)
-        xmin, xres, skx, ymax, sky, yres = driver.GetGeoTransform()
-        return Site(name, epsg, nx, ny, ul, lr, xres, yres)
-
-
-class DEMInfo:
-    def __init__(self, site, dem_full_res):
-        from Common import ImageIO
-        import numpy as np
-        self.epsg = site.epsg_str
-        self.ulx = site.ul[0]
-        self.uly = site.ul[1]
-        self.resx = site.res_x
-        self.resy = site.res_y
-        self.lx = site.px
-        self.ly = site.py
-        self.alt = dem_full_res
-        arr, drv = ImageIO.tiff_to_array(self.alt, array_only=False)
-        self.mean_alt = np.mean(arr)
-        self.std_dev_alt = np.std(arr)
-        self.short_description = ImageIO.get_utm_description(drv)
-        self.nodata = ImageIO.get_nodata_value(drv)
-        res_arr = ImageIO.get_resolution(drv)
-        self.dem_subsampling_ratio = str(int(float(res_arr[0]) / float(site.res_x)))
-
-
 class MNT(object):
     """
     Base class to get the necessary mnt for a given site.
@@ -172,6 +79,7 @@ class MNT(object):
         import tempfile
         from datetime import datetime
         from Common import ImageIO, XMLTools, FileSystem
+        from prepare_mnt.mnt.DEMInfo import DEMInfo
         assert len(resolutions) >= 1
         basename = str("%s_TEST_AUX_REFDE2_%s_%s" % (platform_id, self.site.nom, str(self.dem_version).zfill(4)))
         # Get water data
---
+++ prepare_mnt/mnt/SiteInfo.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) CNES - All Rights Reserved
+This file is subject to the terms and conditions defined in
+file 'LICENSE.md', which is part of this source code package.
+
+Author:         Peter KETTIG <peter.kettig@cnes.fr>, Pierre LASSALLE <pierre.lassalle@cnes.fr>
+Project:        StartMaja, CNES
+Created on:     Tue Sep 11 15:31:00 2018
+"""
+
+surface_water_url = "https://storage.googleapis.com/global-surface-water/downloads2/occurrence/occurrence_%s_v1_1.tif"
+
+
+class Site:
+    """
+    Stores all necessary information in order to create an MNT
+    """
+    def __init__(self, nom, epsg, px, py, ul, lr, res_x, res_y):
+        self.nom = nom
+        self.epsg = epsg
+        self.px = px
+        self.py = py
+        self.res_x = res_x
+        self.res_y = res_y
+        self.ul = ul
+        self.lr = lr
+        self.ul_latlon, self.lr_latlon = self.latlon_minmax
+
+    @property
+    def latlon_minmax(self):
+        """
+        Get lat and lon min and max values
+        :return: latmin, latmax, lonmin, lonmax of the current sites
+        """
+        from Common import ImageIO
+        ul_latlon = ImageIO.transform_point(self.ul, self.epsg, new_epsg=4326)
+        lr_latlon = ImageIO.transform_point(self.lr, self.epsg, new_epsg=4326)
+        return ul_latlon, lr_latlon
+
+    @property
+    def projwin(self):
+        return " ".join([str(self.ul[1]), str(self.ul[0]), str(self.lr[1]), str(self.lr[0])])
+
+    @property
+    def te_str(self):
+        y_val = list(sorted([self.lr[1], self.ul[1]]))
+        x_val = list(sorted([self.lr[0], self.ul[0]]))
+        return " ".join([str(x_val[0]), str(y_val[0]), str(x_val[1]), str(y_val[1])])
+
+    @property
+    def epsg_str(self):
+        return "EPSG:" + str(self.epsg)
+
+    @property
+    def tr_str(self):
+        return str(self.res_x) + " " + str(self.res_y)
+
+    def to_driver(self, path, n_bands=1):
+        from osgeo import osr, gdal
+        # create the raster file
+        dst_ds = gdal.GetDriverByName('GTiff').Create(path, self.py, self.px, n_bands, gdal.GDT_Byte)
+        geotransform = (self.ul[1], self.res_x, 0, self.ul[0], 0, self.res_y)
+        dst_ds.SetGeoTransform(geotransform)  # specify coords
+        srs = osr.SpatialReference()  # establish encoding
+        srs.ImportFromEPSG(self.epsg)  # WGS84 lat/long
+        dst_ds.SetProjection(srs.ExportToWkt())  # export coords to file
+        return dst_ds
+
+    @staticmethod
+    def from_raster(name, raster):
+        """
+        Create site from a raster on disk
+        :param name: The name of the site
+        :param raster: The gdal raster
+        :return: A site class given the infos from the raster.
+        """
+        from Common import ImageIO
+        raster, driver = ImageIO.tiff_to_array(raster, array_only=False)
+        nx, ny = raster.shape[:2]
+        epsg = ImageIO.get_epsg(driver)
+        ul, lr = ImageIO.get_ul_lr(driver)
+        xmin, xres, skx, ymax, sky, yres = driver.GetGeoTransform()
+        return Site(name, epsg, nx, ny, ul, lr, xres, yres)
+
+
