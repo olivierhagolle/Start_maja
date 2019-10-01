e76bb0a1c4c78e1167de4592374b922ed33695c1
Peter KETTIG <peter.kettig@cnes.fr>
2019-10-01 08:38:49 +0200

    Updated to new interfaces of MNT and CAMS files

--- test/Common/test_DummyFiles.py
+++ test/Common/test_DummyFiles.py
@@ -23,151 +23,50 @@ class TestDummyFiles(unittest.TestCase):
     def test_mnt_generation(self):
         import shutil
         gen = DummyFiles.MNTGenerator(self.root)
-        gen.generate()
+        mnt = gen.generate()
         self.assertTrue(gen.platform in self.platforms)
-        self.assertEqual(len(gen.hdr), 1)
-        self.assertEqual(len(gen.dbl), 1)
-        hdr = gen.hdr[0]
-        dbl = gen.dbl[0]
-        self.assertTrue(os.path.exists(hdr))
-        self.assertTrue(os.path.exists(dbl))
-        self.assertTrue("_TEST_AUX_REFDE2_" in hdr)
-        self.assertTrue("_TEST_AUX_REFDE2_" in dbl)
+        self.assertTrue(os.path.exists(mnt.hdr))
+        self.assertTrue(os.path.exists(mnt.dbl))
+        self.assertTrue("_TEST_AUX_REFDE2_" in mnt.hdr)
+        self.assertTrue("_TEST_AUX_REFDE2_" in mnt.dbl)
 
-        shutil.rmtree(dbl)
-        os.remove(hdr)
-        self.assertFalse(os.path.exists(hdr))
-        self.assertFalse(os.path.exists(dbl))
+        shutil.rmtree(mnt.dbl)
+        os.remove(mnt.hdr)
+        self.assertFalse(os.path.exists(mnt.hdr))
+        self.assertFalse(os.path.exists(mnt.dbl))
 
     def test_cams_generation(self):
         import shutil
         gen = DummyFiles.CAMSGenerator(self.root)
-        gen.generate()
+        c = gen.generate()
         self.assertTrue(gen.platform in self.platforms)
-        self.assertEqual(len(gen.hdr), 1)
-        self.assertEqual(len(gen.dbl), 1)
-        hdr = gen.hdr[0]
-        dbl = gen.dbl[0]
-        self.assertTrue(os.path.exists(hdr))
-        self.assertTrue(os.path.exists(dbl))
-        self.assertTrue("_TEST_EXO_CAMS_" in hdr)
-        self.assertTrue("_TEST_EXO_CAMS_" in dbl)
+        self.assertTrue(os.path.exists(c.hdr))
+        self.assertTrue(os.path.exists(c.dbl))
+        self.assertTrue("_TEST_EXO_CAMS_" in c.hdr)
+        self.assertTrue("_TEST_EXO_CAMS_" in c.dbl)
 
-        shutil.rmtree(dbl)
-        os.remove(hdr)
-        self.assertFalse(os.path.exists(hdr))
-        self.assertFalse(os.path.exists(dbl))
+        shutil.rmtree(c.dbl)
+        os.remove(c.hdr)
+        self.assertFalse(os.path.exists(c.hdr))
+        self.assertFalse(os.path.exists(c.dbl))
 
     def test_gipp_generation_cams(self):
-        import shutil
-        from Common import FileSystem
         gen = DummyFiles.GippGenerator(self.root)
-        gen.generate()
+        gipp = gen.generate()
         self.assertTrue(gen.platform in self.platforms)
-
-        lengths_hdr = {"venus": 35, "landsat8": 35, "sentinel2": 69}
-        lengths_dbl = {"venus": 30, "landsat8": 30, "sentinel2": 60}
-
-        self.assertEqual(len(gen.hdr), lengths_hdr[gen.platform])
-        self.assertEqual(len(gen.dbl), lengths_dbl[gen.platform])
-
-        hdr_types = ["ALBD", "DIFT", "DIRT", "TOCR", "WATV"]
-        eef_types = ["COMM", "SITE", "SMAC", "EXTL", "QLTL"]
-        base = os.path.dirname(gen.dbl[0])
-        for ftype in hdr_types:
-            hdr = FileSystem.get_file(root=base, filename="*" + ftype + r"*.HDR")
-            self.assertIsNotNone(hdr)
-            self.assertTrue("_TEST_GIP_" in hdr)
-            dbl = FileSystem.get_file(root=base, filename="*" + ftype + r"*.DBL.DIR")
-            self.assertIsNotNone(dbl)
-            self.assertTrue("_TEST_GIP_" in dbl)
-        for ftype in eef_types:
-            eef = FileSystem.get_file(root=base, filename="*" + ftype + r"*.EEF")
-            self.assertIsNotNone(eef)
-            self.assertTrue("_TEST_GIP_" in eef)
-
-        for dbl in gen.dbl:
-            shutil.rmtree(dbl)
-            self.assertFalse(os.path.exists(dbl))
-        for hdr in gen.hdr:
-            os.remove(hdr)
-            self.assertFalse(os.path.exists(hdr))
+        self.assertTrue(gipp.check_completeness())
 
     def test_gipp_generation_nocams(self):
-        import shutil
-        from Common import FileSystem
         gen = DummyFiles.GippGenerator(self.root)
-        gen.generate(cams=False)
+        gipp = gen.generate(cams=False)
         self.assertTrue(gen.platform in self.platforms)
-
-        lengths_hdr = {"venus": 10, "landsat8": 10, "sentinel2": 19}
-        lengths_dbl = {"venus": 5, "landsat8": 5, "sentinel2": 10}
-
-        self.assertEqual(len(gen.hdr), lengths_hdr[gen.platform])
-        self.assertEqual(len(gen.dbl), lengths_dbl[gen.platform])
-
-        hdr_types = ["ALBD", "DIFT", "DIRT", "TOCR", "WATV"]
-        eef_types = ["COMM", "SITE", "SMAC", "EXTL", "QLTL"]
-        base = os.path.dirname(gen.dbl[0])
-        for ftype in hdr_types:
-            hdr = FileSystem.get_file(root=base, filename="*" + ftype + r"*.HDR")
-            self.assertIsNotNone(hdr)
-            self.assertTrue("_TEST_GIP_" in hdr)
-            dbl = FileSystem.get_file(root=base, filename="*" + ftype + r"*.DBL.DIR")
-            self.assertIsNotNone(dbl)
-            self.assertTrue("_TEST_GIP_" in dbl)
-        for ftype in eef_types:
-            eef = FileSystem.get_file(root=base, filename="*" + ftype + r"*.EEF")
-            self.assertIsNotNone(eef)
-            self.assertTrue("_TEST_GIP_" in eef)
-
-        for dbl in gen.dbl:
-            shutil.rmtree(dbl)
-            self.assertFalse(os.path.exists(dbl))
-        for hdr in gen.hdr:
-            os.remove(hdr)
-            self.assertFalse(os.path.exists(hdr))
+        self.assertTrue(gipp.check_completeness())
 
     def test_gipp_generation_cams_tm(self):
-        import shutil
-        from Common import FileSystem
         gen = DummyFiles.GippGenerator(self.root)
-        gen.generate(mission="tm")
+        gipp = gen.generate(mission="tm")
         self.assertTrue(gen.platform in self.platforms)
-
-        lengths_hdr = {"sentinel2": 75}
-        lengths_dbl = {"sentinel2": 60}
-
-        self.assertEqual(len(gen.hdr), lengths_hdr[gen.platform])
-        self.assertEqual(len(gen.dbl), lengths_dbl[gen.platform])
-
-        hdr_types = ["ALBD", "DIFT", "DIRT", "TOCR", "WATV"]
-        eef_types = ["COMM", "SITE", "SMAC", "EXTL", "QLTL"]
-        base = os.path.dirname(gen.dbl[0])
-        for ftype in hdr_types:
-            hdr = FileSystem.get_file(root=base, filename="*" + ftype + r"*.HDR")
-            self.assertIsNotNone(hdr)
-            self.assertTrue("_TEST_GIP_" in hdr)
-            dbl = FileSystem.get_file(root=base, filename="*" + ftype + r"*.DBL.DIR")
-            self.assertIsNotNone(dbl)
-            self.assertTrue("_TEST_GIP_" in dbl)
-        for ftype in eef_types:
-            eef = FileSystem.get_file(root=base, filename="*" + ftype + r"*.EEF")
-            self.assertIsNotNone(eef)
-            self.assertTrue("_TEST_GIP_" in eef)
-
-        n_l2comm = len(FileSystem.find("*L2COMM*", base))
-        self.assertEqual(n_l2comm, 4)
-        n_qltl = len(FileSystem.find("*CKQLTL*", base))
-        self.assertEqual(n_qltl, 4)
-        n_extl = len(FileSystem.find("*CKEXTL*", base))
-        self.assertEqual(n_extl, 4)
-        for dbl in gen.dbl:
-            shutil.rmtree(dbl)
-            self.assertFalse(os.path.exists(dbl))
-        for hdr in gen.hdr:
-            os.remove(hdr)
-            self.assertFalse(os.path.exists(hdr))
+        self.assertTrue(gipp.check_completeness())
 
     def test_s2_l1_generation(self):
         import shutil
