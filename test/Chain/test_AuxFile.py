23cfd284ced9e27c3c0f344dc3996b4b6989b61e
Peter KETTIG <peter.kettig@cnes.fr>
2019-10-01 08:39:26 +0200

    Updated to dummy file functions

--- test/Chain/test_AuxFile.py
+++ test/Chain/test_AuxFile.py
@@ -17,44 +17,6 @@ import os
 from datetime import datetime
 
 
-def create_dummy_cams(root, date, platform="GEN"):
-    """
-    Create a few dummy CAMS files
-    :param root: The path to create the cams folder in
-    :param date: The start date the cams file is valid for.
-    :param platform: The platform name, e.g. "S2" or "L8"
-    :return: Creates the directory containing the cams folder
-    """
-    end_date = datetime(2099, 1, 1, 23, 59, 59)
-
-    basename = "_".join([platform, "TEST", "EXO", "CAMS",
-                         date.strftime("%Y%m%dT%H%M%S"),
-                         end_date.strftime("%Y%m%dT%H%M%S")])
-    dbl_name = os.path.join(root, basename + ".DBL.DIR")
-    hdr_name = os.path.join(root, basename + ".HDR")
-    os.makedirs(dbl_name)
-    TestFunctions.touch(hdr_name)
-
-
-def create_dummy_mnt(root, tile, platform="GEN"):
-    """
-    Create a dummy mnt with no content specific to a platform and tile
-    :param root: The path to create the mnt in
-    :param tile: The tile identifier
-    :param platform: The platform name, e.g. "S2" or "L8"
-    :return: Returns the directory containing the dummy mnt
-    """
-    import random
-    basename = "_".join([platform, "TEST", "AUX", "REFDE2", tile, str(random.randint(0, 1000)).zfill(4)])
-    mnt_name = os.path.join(root, basename)
-    dbl_name = os.path.join(mnt_name, basename + ".DBL.DIR")
-    hdr_name = os.path.join(mnt_name, basename + ".HDR")
-    os.makedirs(mnt_name)
-    os.makedirs(dbl_name)
-    TestFunctions.touch(hdr_name)
-    return mnt_name
-
-
 class TestAuxFile(unittest.TestCase):
     root = os.getcwd()
 
@@ -70,33 +32,27 @@ class TestAuxFile(unittest.TestCase):
         Similar to test_FileSystem.
         :return:
         """
-        from random import randint, choice
+        from Common import DummyFiles
         os.makedirs(cls.subdir_prefix)
         for i in range(cls.n_cams):
-            d = datetime(year=randint(2010, 2050),
-                         month=randint(1, 12),
-                         day=randint(1, 25),
-                         hour=randint(1, 23),
-                         minute=randint(1, 59))
-            platform = choice(["S2_", "L8_", "GEN", "VE_"])
-            create_dummy_cams(cls.subdir_prefix, d, platform)
+            DummyFiles.CAMSGenerator(cls.subdir_prefix).generate()
 
         for tile in cls.tiles:
-            platform = choice(["S2_", "L8_", "GEN", "VE_"])
-            cls.mnt += [create_dummy_mnt(cls.root, tile, platform)]
+            cls.mnt += [DummyFiles.MNTGenerator(cls.root, tile=tile).generate()]
 
     @classmethod
     def tearDownClass(cls):
         import shutil
         shutil.rmtree(cls.subdir_prefix)
         for m in cls.mnt:
-            shutil.rmtree(m)
+            shutil.rmtree(m.dbl)
+            os.remove(m.hdr)
 
     def test_cams_reg(self):
         import re
-        cams = ["S2__PROD_EXO_CAMS_20171008T030000_20180628T174612.HDR",
-                "VE__TEST_EXO_CAMS_20171008T150000_20180628T174630.DBL.DIR",
-                "L8__TEST_EXO_CAMS_20171010T030000_20180628T174724.DBL"]
+        cams = ["S2__PROD_EXO_CAMS_20171008T030000_20180628T174612.DBL",
+                "VE_TEST_EXO_CAMS_20171008T150000_20180628T174630.DBL.DIR",
+                "L8_TEST_EXO_CAMS_20171010T030000_20180628T174724.DBL"]
         for f in cams:
             self.assertTrue(re.search(CAMSFile.regex, f))
 
@@ -120,27 +76,24 @@ class TestAuxFile(unittest.TestCase):
 
     def test_wrong_cams_creation(self):
         from Common import FileSystem
-        dbl = FileSystem.find("DBL.DIR", self.mnt[0])[0]
+        dbl = FileSystem.find("*AUX_REFDE2*DBL.DIR", self.root)[0]
         self.assertIsNone(CAMSFile(dbl))
 
     def test_cams_date(self):
         from Common import FileSystem
 
-        dbl = FileSystem.find("DBL.DIR", self.subdir_prefix)[0]
+        dbl = FileSystem.find("*EXO_CAMS*DBL.DIR", self.subdir_prefix)[0]
         c = CAMSFile(dbl)
         base = os.path.basename(dbl).split(".")[0]
         date = datetime.strptime(base.split("_")[-2], "%Y%m%dT%H%M%S")
         self.assertEqual(c.get_date(), date)
 
     def test_mnt_creation(self):
-        from Common import FileSystem
         for m in self.mnt:
-            dbl = FileSystem.find("DBL.DIR", m)[0]
-            d = DTMFile(dbl)
-            self.assertIsNotNone(d)
-            base = os.path.basename(dbl).split(".")[0]
-            hdr = os.path.join(os.path.dirname(dbl), base + ".HDR")
-            self.assertEqual(hdr, d.hdr)
+            self.assertIsNotNone(m)
+            base = os.path.basename(m.dbl).split(".")[0]
+            hdr = os.path.join(os.path.dirname(m.dbl), base + ".HDR")
+            self.assertEqual(hdr, m.hdr)
 
     def test_wrong_mnt_creation(self):
         from Common import FileSystem
