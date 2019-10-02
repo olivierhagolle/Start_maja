ee74659542d9469f651711ecd36457f77a968253
Peter KETTIG <peter.kettig@cnes.fr>
2019-10-02 11:19:10 +0200

    gdal_retile test now has a permanent 1.0 tolerance

--- test/Common/test_ImageIO.py
+++ test/Common/test_ImageIO.py
@@ -253,7 +253,6 @@ class TestImageIO(unittest.TestCase):
         FileSystem.remove_file(path)
 
     def test_gdal_tile_untile(self):
-        import sys
         from Common import FileSystem
         img = np.arange(0., 100.).reshape((10, 10))
         path = os.path.join(os.getcwd(), "test_gdal_retile.tif")
@@ -267,24 +266,18 @@ class TestImageIO(unittest.TestCase):
         expected = np.array([[88, 89],
                              [98, 99]])
 
-        if sys.version_info >= (3, 6):
-            np.testing.assert_array_almost_equal(expected, img_read)
-        else:
-            # gdal_retile for python 3.5 is producing the following image:
-            # [[87, 89], [97, 99]].
-            # To account for this, a maximum absolute error of 1.0 is allowed during testing
-            np.testing.assert_allclose(expected, img_read, atol=1.0)
+        # Some gdal_retile versions are producing the following image:
+        # [[87, 89], [97, 99]].
+        # To account for this, a maximum absolute error of 1.0 is allowed during testing
+        np.testing.assert_allclose(expected, img_read, atol=1.0)
 
         # Untile
         untiled = os.path.join(os.getcwd(), "test_gdal_untile.tif")
         ImageIO.gdal_merge(untiled, *tiles)
         self.assertTrue(os.path.exists(untiled))
         img_combined, _ = ImageIO.tiff_to_array(untiled)
-        if sys.version_info >= (3, 6):
-            np.testing.assert_array_almost_equal(img, img_combined)
-        else:
-            # Same as the comment above
-            np.testing.assert_allclose(img, img_combined, atol=1.0)
+        # Same as the comment above
+        np.testing.assert_allclose(img, img_combined, atol=1.0)
 
         FileSystem.remove_file(path)
         FileSystem.remove_file(untiled)
